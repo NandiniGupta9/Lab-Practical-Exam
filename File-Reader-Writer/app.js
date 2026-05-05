2ee2e149347b5ef6a968eb9603b0eb8fc174b946
NandiniGupta9 <nandinigupta8354@gmail.com>
2026-05-05 12:15:54 +0530

    [Nandini]Added code

--- File-Reader-Writer/app.js
+++ File-Reader-Writer/app.js
@@ -5,7 +5,7 @@ fs.readFile('input.txt','utf8',(err,data)=>{
 
     data=data.trim();
     const lines=data.split('\n').length;
-    const words=data.split('/\+s/'.length);
+    const words=data.split('/\s+/'.length);
     fs.writeFile('output.txt',`Total Lines: ${lines}\nTotal Words: ${words}`, () => {
         console.log("Done");
     });
